b7000f37a5218542fd97b9e020570c15e7e4d75a
Nico <nico.studt@student.uni-luebeck.de>
2020-04-17 22:54:04 +0200

    JoystickDetailView Update

---
+++ app/src/main/java/com/schneewittchen/rosandroid/ui/custum_views/ScaleView.java
@@ -0,0 +1,13 @@
+package com.schneewittchen.rosandroid.ui.custum_views;
+
+/**
+ * TODO: Description
+ *
+ * @author Nico Studt
+ * @version 1.0.0
+ * @created on 17.04.20
+ * @updated on 17.04.20
+ * @modified by
+ */
+public class ScaleView {
+}
